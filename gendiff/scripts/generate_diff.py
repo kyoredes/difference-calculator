452799558328fc23d30a5b822115997204b24bc7
ddertaliss <ddertaliss@gmail.com>
2023-10-21 22:18:20 +1000

    add generate_diff script

--- gendiff/scripts/generate_diff.py
+++ gendiff/scripts/generate_diff.py
@@ -3,16 +3,25 @@ import json
 
 def main(file1, file2):
     file1, file2 = return_json(file1), return_json(file2)
+    # file1 = '-'
+    # file2 = '+'
+    value1, value2 = list(file1.values()), list(file2.values())
+    res = ''
     dictionary = merge(file1, file2)
     dictionary = dict(sorted(dictionary.items()))
-    res = []
-    for element in dictionary:
-        if ele
-            
-        
-    
-        
-        
+    for key in dictionary:
+        # only value1
+        if dictionary[key] in value1 and dictionary[key] not in value2:
+            res += f'   - {key}: {dictionary[key]}\n'
+            continue
+        # only value2
+        if dictionary[key] in value2 and dictionary[key] not in value1:
+            res += f'   + {key}: {dictionary[key]}\n'
+            continue
+        if dictionary[key] in value1 and dictionary[key] in value2:
+            res += f'   {key}: {dictionary[key]}\n'
+            continue
+    return f'{{\n{res}\n}}'
 
 
 def return_json(file):
@@ -23,7 +32,3 @@ def merge(file1, file2):
     res = file1.copy()
     res.update(file2)
     return res
-
-
-def flatten(dictionary):
-    return [x for y in dictionary for x in y]
