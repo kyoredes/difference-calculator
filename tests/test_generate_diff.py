441eb2beb3984d61a8796cb9cbbc830f110b581d
ddertaliss <ddertaliss@gmail.com>
2023-11-01 20:02:52 +1000

    add teests/fixtures

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,1 +1,15 @@
-import 
+# import pytest
+from gendiff.scripts.generate_diff import main
+import tests.fixtures as fixtures
+
+
+# @pytest.fixture
+def file1():
+    return fixtures.file1
+
+
+# @pytest.fixture
+def file2():
+    pass
+
+print(file1)
