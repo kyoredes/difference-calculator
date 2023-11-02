0aac33cbc642a5b5b19cb9bf7a1ad1a81a856ecf
ddertaliss <ddertaliss@gmail.com>
2023-11-02 08:39:56 +0300

    add pytest

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,15 +1,26 @@
-# import pytest
-from gendiff.scripts.generate_diff import main
+import pytest
+import json
+from gendiff import generate_diff
 import tests.fixtures as fixtures
 
 
-# @pytest.fixture
+@pytest.fixture
 def file1():
-    return fixtures.file1
+    return json.load(open(fixtures.file1))
 
 
-# @pytest.fixture
+@pytest.fixture
 def file2():
-    pass
+    return json.load(open(fixtures.file2))
 
-print(file1)
+
+def test_main(file1, file2):
+    correct = '{
+    - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}'
+
