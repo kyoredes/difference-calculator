44f2f0794652eb5c8ed5c71fa3685e311be25413
ddertaliss <ddertaliss@gmail.com>
2023-11-03 14:56:58 +0300

    test

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,19 +1,20 @@
-import pytest
-import json
 from gendiff import generate_diff
 import tests.fixtures as fixtures
 
 
-@pytest.fixture
 def file1():
-    return json.load(open(fixtures.file1))
+    return 'python-project-50/tests/fixtures/file1.json'
 
 
-@pytest.fixture
 def file2():
-    return json.load(open(fixtures.file2))
+    return 'python-project-50/tests/fixtures/file2.json'
 
 
-def test_main(file1, file2):
+def test_main():
     correct = "{\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n}"  # noqa: E501
-    assert generate_diff(file1, file2) == correct
+    assert generate_diff(file1(), file2()) == correct
+
+
+# test_main(file1, file2)
+# def test():
+#    assert file1 == file2
