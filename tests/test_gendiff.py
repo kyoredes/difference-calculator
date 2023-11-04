d62456472114702e6043449a1ed464d89d51d378
ddertaliss <ddertaliss@gmail.com>
2023-11-04 14:04:10 +1000

    test

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,7 +1,11 @@
+import os
 from gendiff.scripts import generate_diff
-from tests.fixtures import file1
-from tests.fixtures import file2
-from tests.fixtures import correct
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+path1 = f'{current_dir}/fixtures/file1.json'
+path2 = f'{current_dir}/fixtures/file2.json'
+correct = open(f'{current_dir}/fixtures/correct.txt').read()
+
 
 def test_gendiff():
-    assert generate_diff(file1, file2) == correct
+    assert generate_diff(path1, path2) == correct
