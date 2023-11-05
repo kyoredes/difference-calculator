6e069d43d870ff8aa10b1d9a1980b8f08de06f6e
ddertaliss <ddertaliss@gmail.com>
2023-11-05 16:09:39 +1000

    lint

--- gendiff/scripts/test.py
+++ gendiff/scripts/test.py
@@ -1,4 +1,3 @@
 from generate_diff import diff
-path1, path2 = '/python-project-50/gendiff/scripts/json-files/file1.json', '/python-project-50/gendiff/scripts/json-files/file2.json'
+path1, path2 = '/python-project-50/gendiff/scripts/json-files/file1.json', '/python-project-50/gendiff/scripts/json-files/file2.json'  # noqa: E501
 print(diff(path1, path2))
-
