2f371d67f45222842e10661fc81c296deb16bfa5
ddertaliss <ddertaliss@gmail.com>
2024-02-21 11:44:35 +0300

    repo cleaned

--- gendiff/scripts/test.py
+++
@@ -1,3 +0,0 @@
-from generate_diff import diff
-path1, path2 = '/python-project-50/gendiff/scripts/json-files/file1.json', '/python-project-50/gendiff/scripts/json-files/file2.json'  # noqa: E501
-print(diff(path1, path2))
